36f7f167c9c9ab0e34c82c8e4520a6fc03f857cc
Denis Yilmaz <denis@ynm.studio>
2022-01-21 13:42:56 +0100

    add nice fade-in/out effect

--- frontend/projects/application-tool/src/app/app.component.ts
+++ frontend/projects/application-tool/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, Renderer2 } from '@angular/core';
+import { Event, NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { AuthService } from './shared/services/auth.service';
 import { MultilangService } from './shared/services/multilang.service';
 
@@ -8,12 +10,32 @@ import { MultilangService } from './shared/services/multilang.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'application-tool';
+  @HostListener('window:beforeunload', ['$event'])
+  unloadHandler(event: Event) {
+    // Your logic on beforeunload
+    this.renderer.addClass(this.document.body, 'fade-out');
+  }
+  initialized: boolean = false;
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private router: Router,
     public authService: AuthService,
     public multilangService: MultilangService
-  ) {}
+  ) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        // console.warn("event instanceof NavigationStart");
+      }
+      if (event instanceof NavigationEnd) {
+        if (!this.initialized) {
+          this.renderer.removeClass(this.document.body, 'fade-out');
+          this.initialized = true;
+        }
+      }
+    });
+  }
 
   sendVerification() {
     this.authService.sendEmailVerification();
