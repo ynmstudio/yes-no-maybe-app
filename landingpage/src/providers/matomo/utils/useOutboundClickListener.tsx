5a743ecd274d03d4bf7254e90bb227773fe14d63
Denis Yılmaz <denis@ynm.studio>
2024-02-28 14:45:07 +0100

    fix: update useOutboundClickListener()

--- landingpage/src/providers/matomo/utils/useOutboundClickListener.tsx
+++ landingpage/src/providers/matomo/utils/useOutboundClickListener.tsx
@@ -2,48 +2,46 @@ import { useOnDocument, $, useContext } from "@builder.io/qwik";
 import MatomoContext from "../MatomoContext";
 
 function useOutboundClickListener() {
-    const { instance } = useContext(MatomoContext);
+  const { matomo } = useContext(MatomoContext);
 
-    const handleOutboundClick = $((event: Event) => {
-        // The target is not guaranteed to be a link, it could be a child element.
-        // Look up the element's parent until the anchor element is found.
-        const findLinkElement = (
-            el: EventTarget | null
-        ): HTMLElement | null => {
-            if (el instanceof HTMLAnchorElement && el.href) {
-                return el;
-            }
-            if (el instanceof HTMLElement && el.parentElement) {
-                return findLinkElement(el.parentElement);
-            }
-            return null;
-        };
+  const handleOutboundClick = $((event: Event) => {
+    // The target is not guaranteed to be a link, it could be a child element.
+    // Look up the element's parent until the anchor element is found.
+    const findLinkElement = (el: EventTarget | null): HTMLElement | null => {
+      if (el instanceof HTMLAnchorElement && el.href) {
+        return el;
+      }
+      if (el instanceof HTMLElement && el.parentElement) {
+        return findLinkElement(el.parentElement);
+      }
+      return null;
+    };
 
-        const target = findLinkElement(event.target);
+    const target = findLinkElement(event.target);
 
-        if (!(target instanceof HTMLAnchorElement)) {
-            return;
-        }
+    if (!(target instanceof HTMLAnchorElement)) {
+      return;
+    }
 
-        const { href } = target;
+    const { href } = target;
 
-        // Check if the click target differs from the current hostname, meaning it's external
-        if (
-            /* @ts-ignore */
-            !href.match(
-                new RegExp(
-                    `^(http://www.|https://www.|http://|https://)+(${window.location.hostname})`
-                )
-            )
-        ) {
-            instance?.trackLink({ href });
-        }
-    });
+    // Check if the click target differs from the current hostname, meaning it's external
+    if (
+      /* @ts-ignore */
+      !href.match(
+        new RegExp(
+          `^(http://www.|https://www.|http://|https://)+(${window.location.hostname})`,
+        ),
+      )
+    ) {
+      matomo?.trackLink({ href });
+    }
+  });
 
-    useOnDocument(
-        "click",
-        $((event) => handleOutboundClick(event))
-    );
+  useOnDocument(
+    "click",
+    $((event) => handleOutboundClick(event)),
+  );
 }
 
 export default useOutboundClickListener;
